84ac66e0fce5d81a320c81c53d21aa58831df298
Tony Buckner <tony.buckner26@gmail.com>
2024-11-04 10:09:53 -0500

    Initial commit

---
+++ Fetch-Recipe-Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch-Recipe-Viewer
+//
+//  Created by Tony Buckner on 11/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch-Recipe-Viewer/Fetch_Recipe_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Recipe_ViewerApp.swift
+//  Fetch-Recipe-Viewer
+//
+//  Created by Tony Buckner on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Recipe_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
